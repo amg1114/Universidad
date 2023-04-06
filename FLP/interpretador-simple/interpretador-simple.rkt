925f971b6f355d7f6685bac90aac39df8d202af4
Alejandro Moreno <alejandromogil@gmail.com>
2023-04-05 19:56:52 -0500

    Interpretador Simple: Literales y Variables

--- FLP/interpretador-simple/interpretador-simple.rkt
+++ FLP/interpretador-simple/interpretador-simple.rkt
@@ -29,18 +29,83 @@
     )
   )
 
-(sllgen:make-define-datatypes especificacion-lexica especificacion-gramatical)
-
 ;; Interpretador
 (define eval-program
-  (lambda (program)
-    program
+  (lambda (prog)
+    (cases programa prog
+      (a-program (exp) (evaluar-expresion exp ambiente-inicial))
+      )
     )
   )
+;; Funcion de Trabajo
+(define evaluar-expresion
+  (lambda (exp ambiente)
+    (cases expresion exp
+      (lit-exp (dato) dato)
+      (var-exp (id) (apply-env ambiente id))
+      (else "ok")
+      )
+    )
+  )
+
+
+(sllgen:make-define-datatypes especificacion-lexica especificacion-gramatical)
+
 
 (define interpretador
   (sllgen:make-rep-loop ">" eval-program
                         (sllgen:make-stream-parser especificacion-lexica especificacion-gramatical)
                         )
   )
+
+
+;; Ambientes
+(define-datatype ambiente ambiente?
+  (ambiente-vacio )
+  (ambiente-extendido
+   (identificadores (list-of symbol?))
+   (valores (list-of valor?))
+   (ambiente-ex ambiente?)
+   )
+  )
+(define valor?
+  (lambda (x)
+    #t
+    )
+  )
+
+(define apply-env
+  (lambda (env sym)
+    (cases ambiente env
+      (ambiente-vacio () (eopl:error "Variable " sym " no Encontrada"))
+      (ambiente-extendido (lid lval env-old)
+                          (letrec
+                              (
+                               (buscar-sym
+                                (lambda (lid lval sym)
+                                  (cond
+                                    [(null? lid) (apply-env env-old sym)]
+                                    [(equal? (car lid) sym) (car lval)]
+                                    [else (buscar-sym (cdr lid) (cdr lval) sym)]
+                                    )
+                                  )
+                                )
+                               )
+                            (buscar-sym lid lval sym)
+                            )
+                          )
+      )
+    )
+  )
+
+;; Ambiente Inicial
+(define ambiente-inicial
+  (ambiente-extendido '(x y z) '(1 2 3)
+                      (ambiente-extendido '(a b c) '(3 2 1)
+                                          (ambiente-vacio)
+                                          )
+                      )
+  )
+
+
 (interpretador)
